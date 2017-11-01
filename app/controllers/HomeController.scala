986a4e3bbff76567aa3743048dcf0bf2ff9949d5
Andrew Zhang <me@andrewmzhang.com>
2017-10-31 21:10:08 -0700

    Setting up file upload

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -1,24 +1,113 @@
 package controllers
 
 import javax.inject._
+
 import play.api._
+import play.api.data.Form
+import play.api.mvc.MultipartFormData.FilePart
 import play.api.mvc._
+import play.core.parsers.Multipart.FileInfo
+
+import java.io.File
+import java.nio.file.{Files, Path}
+import javax.inject._
+
+import akka.stream.IOResult
+import akka.stream.scaladsl._
+import akka.util.ByteString
+import play.api._
+import play.api.data.Form
+import play.api.data.Forms._
+import play.api.libs.streams._
+import play.api.mvc.MultipartFormData.FilePart
+import play.api.mvc._
+import play.core.parsers.Multipart.FileInfo
+
+import scala.concurrent.{ExecutionContext, Future}
+
+case class FormData(name: String)
 
 /**
  * This controller creates an `Action` to handle HTTP requests to the
  * application's home page.
  */
 @Singleton
-class HomeController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
+class HomeController @Inject()(cc: MessagesControllerComponents)(implicit executionContext: ExecutionContext) extends MessagesAbstractController(cc) {
+
+
+  private val logger = Logger(this.getClass)
+
+
+
+
+
+  val form = Form(
+    mapping(
+      "name" -> text
+    )(FormData.apply)(FormData.unapply)
+  )
 
   /**
-   * Create an Action to render an HTML page.
-   *
-   * The configuration in the `routes` file means that this method
-   * will be called when the application receives a `GET` request with
-   * a path of `/`.
-   */
-  def index() = Action { implicit request: Request[AnyContent] =>
-    Ok(views.html.index())
+    * Create an Action to render an HTML page.
+    *
+    * The configuration in the `routes` file means that this method
+    * will be called when the application receives a `GET` request with
+    * a path of `/`.
+    */
+  def index() = Action { implicit request =>
+    Ok(views.html.index(form))
   }
+
+
+
+  type FilePartHandler[A] = FileInfo => Accumulator[ByteString, FilePart[A]]
+
+  /**
+    * Uses a custom FilePartHandler to return a type of "File" rather than
+    * using Play's TemporaryFile class.  Deletion must happen explicitly on
+    * completion, rather than TemporaryFile (which uses finalization to
+    * delete temporary files).
+    *
+    * @return
+    */
+  private def handleFilePartAsFile: FilePartHandler[File] = {
+    case FileInfo(partName, filename, contentType) =>
+      val path: Path = Files.createTempFile("multipartBody", "tempFile")
+      val fileSink: Sink[ByteString, Future[IOResult]] = FileIO.toPath(path)
+      val accumulator: Accumulator[ByteString, IOResult] = Accumulator(fileSink)
+      accumulator.map {
+        case IOResult(count, status) =>
+          logger.info(s"count = $count, status = $status")
+          FilePart(partName, filename, contentType, path.toFile)
+      }
+  }
+
+  /**
+    * A generic operation on the temporary file that deletes the temp file after completion.
+    */
+  private def operateOnTempFile(file: File) = {
+    val size = Files.size(file.toPath)
+    logger.info(s"size = ${size}")
+    Files.deleteIfExists(file.toPath)
+    size
+  }
+
+  /**
+    * Uploads a multipart file as a POST request.
+    *
+    * @return
+    */
+  def upload = Action(parse.multipartFormData(handleFilePartAsFile)) { implicit request =>
+    val fileOption = request.body.file("name").map {
+      case FilePart(key, filename, contentType, file) =>
+        logger.info(s"key = ${key}, filename = ${filename}, contentType = ${contentType}, file = $file")
+        val data = operateOnTempFile(file)
+        data
+    }
+
+    Ok(s"file size = ${fileOption.getOrElse("no file")}")
+  }
+
+
+
 }
